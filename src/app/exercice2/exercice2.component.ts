c44e12588079f458dd526c682761e5b5fc3b3a63
hamza <hamza_gaddour@yahoo.com>
2023-04-09 13:13:17 +0100

    projet 2

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-exercice2',
   templateUrl: './exercice2.component.html',
-  styleUrls: ['./exercice2.component.css']
+  styleUrls: ['./exercice2.component.scss']
 })
 export class Exercice2Component implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  pointer = "pointer"
+  exercice2() {
+    console.log("Click");
   }
 
 }
